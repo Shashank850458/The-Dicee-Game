5513a95652e85d54c5c6ebb0580cf132570f0c5a
shashank hebbar <86871679+Shashank850458@users.noreply.github.com>
2023-04-06 16:56:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var randomNumber1=Math.floor(Math.random()*6)+1;//1-6
 var randomDiceImage1= "Dice" + randomNumber1 + ".png"; //Dice1.png-Dice6.png;
-var randomImageSource1="Images/"+randomDiceImage1; //Images/Dice1.png-Images/Dice6.png;
+var randomImageSource1="images/"+randomDiceImage1; //Images/Dice1.png-Images/Dice6.png;
 var image1=document.querySelectorAll("img")[0];
 image1.setAttribute("src", randomImageSource1);
 
 var randomNumber2=Math.floor(Math.random()*6)+1;
-var randomImageSource2="Images/Dice"+randomNumber2+".png";
+var randomImageSource2="images/Dice"+randomNumber2+".png";
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
 //if Player1 Wins
